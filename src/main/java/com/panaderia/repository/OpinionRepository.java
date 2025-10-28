9f4ac5c53f0fdddb5c14b8311aa8fd38484b5844
Sanchez Morales Jairo <165832316+SanchezMRL@users.noreply.github.com>
2025-10-28 18:08:28 -0500

    Update OpinionRepository.java

--- src/main/java/com/panaderia/repository/OpinionRepository.java
+++ src/main/java/com/panaderia/repository/OpinionRepository.java
@@ -1,9 +1,16 @@
 package com.panaderia.repository;
 
-import com.panaderia.entity.Opinion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.panaderia.entity.OpinionPedido;
+import java.util.List;
 
 @Repository
-public interface OpinionRepository extends JpaRepository<Opinion, Long> {
+public interface OpinionRepository extends JpaRepository<OpinionPedido, Integer> {
+
+    // 🔍 Buscar opiniones por pedido
+    List<OpinionPedido> findByPedidoCliente_IdPedidoCliente(Integer idPedidoCliente);
+
+    // 🔍 Buscar opiniones por cliente
+    List<OpinionPedido> findByCliente_IdCliente(Integer idCliente);
 }
