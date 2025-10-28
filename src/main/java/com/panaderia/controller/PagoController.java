f180740adcd83afece22d271aba455129571f56d
Sanchez Morales Jairo <165832316+SanchezMRL@users.noreply.github.com>
2025-10-28 18:48:15 -0500

    Update PagoController.java

--- src/main/java/com/panaderia/controller/PagoController.java
+++ src/main/java/com/panaderia/controller/PagoController.java
@@ -1,10 +1,18 @@
 package com.panaderia.controller;
 
-import com.panaderia.entity.*;
-import com.panaderia.repository.*;
+import com.panaderia.entity.Pago;
+import com.panaderia.entity.PedidoCliente;
+import com.panaderia.entity.MetodoPago;
+import com.panaderia.entity.EstadoPago;
+import com.panaderia.repository.PagoRepository;
+import com.panaderia.repository.PedidoClienteRepository;
+import com.panaderia.repository.MetodoPagoRepository;
+import com.panaderia.repository.EstadoPagoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import java.time.LocalDate;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Map;
 
 @RestController
@@ -13,25 +21,28 @@ public class PagoController {
 
     @Autowired private PagoRepository pagoRepo;
     @Autowired private PedidoClienteRepository pedidoRepo;
-    @Autowired private MetodoPagoRepository metodoPagoRepo;
-    @Autowired private EstadoPagoRepository estadoPagoRepo;
+    @Autowired private MetodoPagoRepository metodoRepo;
+    @Autowired private EstadoPagoRepository estadoRepo;
 
     @PostMapping
-public Map<String, Object> registrarPago(@RequestBody Map<String, Object> datos) {
-    Long idPedido = Long.valueOf(datos.get("id_pedido_cliente").toString());
-    Long idMetodo = Long.valueOf(datos.get("id_metodo_pago").toString());
-    Long idEstado = Long.valueOf(datos.get("id_estado_pago").toString());
+    public Map<String, Object> registrarPago(@RequestBody Map<String, Object> datos) {
+        Long idPedido = Long.valueOf(datos.get("id_pedido_cliente").toString());
+        Long idMetodo = Long.valueOf(datos.get("id_metodo_pago").toString());
+        Long idEstado = Long.valueOf(datos.get("id_estado_pago").toString());
 
-    PedidoCliente pedido = pedidoRepo.findById(idPedido).orElse(null);
+        PedidoCliente pedido = pedidoRepo.findById(idPedido).orElse(null);
+        MetodoPago metodo = metodoRepo.findById(idMetodo).orElse(null);
+        EstadoPago estado = estadoRepo.findById(idEstado).orElse(null);
 
-    Pago pago = new Pago();
-    pago.setPedidoCliente(pedido);
-    pago.setMonto(new BigDecimal(datos.get("monto").toString()));
-    pago.setMetodoPago(metodoRepo.findById(idMetodo).orElse(null));
-    pago.setEstadoPago(estadoRepo.findById(idEstado).orElse(null));
-    pago.setFecha(LocalDateTime.now());
+        Pago pago = new Pago();
+        pago.setPedidoCliente(pedido);
+        pago.setMetodoPago(metodo);
+        pago.setEstadoPago(estado);
+        pago.setMonto(new BigDecimal(datos.get("monto").toString()));
+        pago.setFecha(LocalDateTime.now());
 
-    pagoRepo.save(pago);
-    return Map.of("id_pago", pago.getId_pago());
+        pagoRepo.save(pago);
+
+        return Map.of("id_pago", pago.getId_pago());
+    }
 }
-
