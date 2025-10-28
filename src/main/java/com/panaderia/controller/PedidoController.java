8a32aab97ba020fddce2a1dd6a45317e78cb7e55
Sanchez Morales Jairo <165832316+SanchezMRL@users.noreply.github.com>
2025-10-28 17:47:27 -0500

    Create PedidoController.java

---
+++ src/main/java/com/panaderia/controller/PedidoController.java
@@ -0,0 +1,32 @@
+package com.panaderia.controller;
+
+import com.panaderia.entity.*;
+import com.panaderia.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/api/pedido")
+public class PedidoController {
+
+    @Autowired private PedidoClienteRepository pedidoRepo;
+    @Autowired private ClienteRepository clienteRepo;
+    @Autowired private EmpleadoRepository empleadoRepo;
+    @Autowired private ProductoRepository productoRepo;
+
+    @PostMapping
+    @Transactional
+    public Map<String, Object> registrarPedido(@RequestBody PedidoCliente pedido) {
+        pedido.setFecha(LocalDate.now());
+        pedido.getDetalles().forEach(det -> {
+            det.setPedidoCliente(pedido);
+            det.setProducto(productoRepo.findById(det.getProducto().getId_producto()).orElse(null));
+        });
+        PedidoCliente guardado = pedidoRepo.save(pedido);
+        return Map.of("id_pedido_cliente", guardado.getId_pedido_cliente());
+    }
+}
