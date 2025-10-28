5ef7aa217e4749cb601269701ff73c3f96e73a0c
Sanchez Morales Jairo <165832316+SanchezMRL@users.noreply.github.com>
2025-10-28 18:19:20 -0500

    Update MetodoPago.java

--- src/main/java/com/panaderia/entity/MetodoPago.java
+++ src/main/java/com/panaderia/entity/MetodoPago.java
@@ -1,7 +1,21 @@
 package com.panaderia.entity;
 
-public enum MetodoPago {
-    EFECTIVO,
-    TARJETA,
-    TRANSFERENCIA
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "metodopago")
+public class MetodoPago {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idMetodoPago;
+
+    private String nombre;
+
+    // Getters y Setters
+    public Integer getIdMetodoPago() { return idMetodoPago; }
+    public void setIdMetodoPago(Integer idMetodoPago) { this.idMetodoPago = idMetodoPago; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
 }
