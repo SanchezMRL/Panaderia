16909f97a751d85facf1cdb792d97be160326d63
Sanchez Morales Jairo <165832316+SanchezMRL@users.noreply.github.com>
2025-10-28 17:47:49 -0500

    Update PagoController.java

--- src/main/java/com/panaderia/controller/PagoController.java
+++ src/main/java/com/panaderia/controller/PagoController.java
@@ -1,25 +1,33 @@
 package com.panaderia.controller;
 
 import com.panaderia.entity.Pago;
-import com.panaderia.service.PagoService;
+import com.panaderia.entity.PedidoCliente;
+import com.panaderia.repository.PagoRepository;
+import com.panaderia.repository.PedidoClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import java.time.LocalDate;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/pago")
+@RequestMapping("/api/pago")
 public class PagoController {
 
-    @Autowired
-    private PagoService pagoService;
+    @Autowired private PagoRepository pagoRepo;
+    @Autowired private PedidoClienteRepository pedidoRepo;
 
     @PostMapping
-    public String registrarPago(@RequestBody Pago pago) {
-        try {
-            pagoService.registrarPago(pago);
-            return "{\"ok\": true}";
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "{\"ok\": false}";
-        }
+    public Map<String, Object> registrarPago(@RequestBody Map<String, Object> datos) {
+        Integer idPedido = (Integer) datos.get("id_pedido_cliente");
+        PedidoCliente pedido = pedidoRepo.findById(idPedido).orElse(null);
+
+        Pago pago = new Pago();
+        pago.setPedidoCliente(pedido);
+        pago.setMonto(new java.math.BigDecimal(datos.get("monto").toString()));
+        pago.setMetodo((String) datos.get("metodo"));
+        pago.setFecha(LocalDate.now());
+        pago.setEstado("pendiente");
+        pagoRepo.save(pago);
+        return Map.of("id_pago", pago.getId_pago());
     }
 }
