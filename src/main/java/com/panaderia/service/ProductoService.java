44af53f324d8ed5e67be2bb85d051e78e1b33ea6
Sanchez Morales Jairo <165832316+SanchezMRL@users.noreply.github.com>
2025-10-27 19:32:51 -0500

    Update ProductoService.java

--- src/main/java/com/panaderia/service/ProductoService.java
+++ src/main/java/com/panaderia/service/ProductoService.java
@@ -1,1 +1,30 @@
-.......
+package com.panaderia.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.panaderia.repository.ProductoRepository;
+import com.panaderia.model.Producto;
+import java.util.List;
+
+@Service
+public class ProductoService {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    public List<Producto> listarProductos() {
+        return productoRepository.findAll();
+    }
+
+    public void guardarProducto(Producto producto) {
+        productoRepository.save(producto);
+    }
+
+    public void eliminarProducto(Long id) {
+        productoRepository.deleteById(id);
+    }
+
+    public Producto buscarPorId(Long id) {
+        return productoRepository.findById(id).orElse(null);
+    }
+}
