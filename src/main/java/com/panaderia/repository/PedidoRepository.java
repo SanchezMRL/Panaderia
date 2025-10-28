eaf3ef7cbce6bcb5133e096fdc83c0c986a45579
Sanchez Morales Jairo <165832316+SanchezMRL@users.noreply.github.com>
2025-10-28 17:52:11 -0500

    Delete src/main/java/com/panaderia/repository/PedidoRepository.java

--- src/main/java/com/panaderia/repository/PedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.panaderia.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.panaderia.entity.Pedido;
-
-@Repository
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-}
