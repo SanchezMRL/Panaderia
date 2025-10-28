d49084ec6d9eb7c20ecd5ec824a662aefe1a9598
Sanchez Morales Jairo <165832316+SanchezMRL@users.noreply.github.com>
2025-10-28 18:55:53 -0500

    Update OpinionController.java

--- src/main/java/com/panaderia/controller/OpinionController.java
+++ src/main/java/com/panaderia/controller/OpinionController.java
@@ -4,6 +4,8 @@ import com.panaderia.entity.*;
 import com.panaderia.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.Map;
 
@@ -16,20 +18,19 @@ public class OpinionController {
     @Autowired private PedidoClienteRepository pedidoRepo;
 
     @PostMapping
-public Map<String, Object> registrarOpinion(@RequestBody Map<String, Object> datos) {
-    Long idPedido = Long.valueOf(datos.get("id_pedido_cliente").toString());
-    Long idCliente = Long.valueOf(datos.get("id_cliente").toString());
+    public Map<String, Object> registrarOpinion(@RequestBody Map<String, Object> datos) {
+        Long idPedido = Long.valueOf(datos.get("id_pedido_cliente").toString());
+        Long idCliente = Long.valueOf(datos.get("id_cliente").toString());
 
-    OpinionPedido op = new OpinionPedido();
-    op.setPedidoCliente(pedidoRepo.findById(idPedido).orElse(null));
-    op.setCliente(clienteRepo.findById(idCliente).orElse(null));
-    op.setComentario((String) datos.get("comentario"));
-    op.setCalificacion(Integer.valueOf(datos.get("calificacion").toString()));
-    op.setSatisfaccion(Integer.valueOf(datos.get("satisfaccion").toString()));
-    op.setFecha(Timestamp.valueOf(LocalDateTime.now()));
+        OpinionPedido op = new OpinionPedido();
+        op.setPedidoCliente(pedidoRepo.findById(idPedido).orElse(null));
+        op.setCliente(clienteRepo.findById(idCliente).orElse(null));
+        op.setComentario((String) datos.get("comentario"));
+        op.setCalificacion(Integer.valueOf(datos.get("calificacion").toString()));
+        op.setSatisfaccion(Integer.valueOf(datos.get("satisfaccion").toString()));
+        op.setFecha(Timestamp.valueOf(LocalDateTime.now()));
 
-    opinionRepo.save(op);
-    return Map.of("id_opinion", op.getId_opinion());
-}
-
+        opinionRepo.save(op);
+        return Map.of("id_opinion", op.getId_opinion());
+    }
 }
