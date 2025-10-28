ffa7f116b2503f6d8574cda13a79691776e17bc4
Sanchez Morales Jairo <165832316+SanchezMRL@users.noreply.github.com>
2025-10-27 21:49:15 -0500

    Implement LoginService for user authentication

---
+++ src/main/java/com/panaderia/service/LoginService.java
@@ -0,0 +1,27 @@
+package com.panaderia.service;
+
+import com.panaderia.entity.Cliente;
+import com.panaderia.entity.Empleado;
+import com.panaderia.repository.ClienteRepository;
+import com.panaderia.repository.EmpleadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    private EmpleadoRepository empleadoRepository;
+
+    public Object autenticar(String usuario, String password, String tipo) {
+        if ("cliente".equalsIgnoreCase(tipo)) {
+            return clienteRepository.findByEmailAndPassword(usuario, password);
+        } else if ("admin".equalsIgnoreCase(tipo)) {
+            return empleadoRepository.findByNombreAndPassword(usuario, password);
+        }
+        return null;
+    }
+}
