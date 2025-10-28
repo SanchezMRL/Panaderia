46b0654ee682c91814a5ef4efa9d35383dd146be
Sanchez Morales Jairo <165832316+SanchezMRL@users.noreply.github.com>
2025-10-27 19:48:14 -0500

    Add PedidoRepository interface for Pedido entity

--- src/main/java/com/panaderia/repository/PedidoRepository.java
+++ src/main/java/com/panaderia/repository/PedidoRepository.java
@@ -1,1 +1,9 @@
-.
+package com.panaderia.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.panaderia.entity.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+}
