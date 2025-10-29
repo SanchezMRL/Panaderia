0d04c95759ea9244e092f6a5ff4d79a8dcf45ade
Sanchez Morales Jairo <165832316+SanchezMRL@users.noreply.github.com>
2025-10-28 19:09:49 -0500

    Update Producto.java

--- src/main/java/com/panaderia/entity/Producto.java
+++ src/main/java/com/panaderia/entity/Producto.java
@@ -11,4 +11,29 @@ public class Producto {
 
     private String nombre;
     private Double precio_base;
+
+    // ✅ Getters y Setters
+    public Long getId_producto() {
+        return id_producto;
+    }
+
+    public void setId_producto(Long id_producto) {
+        this.id_producto = id_producto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio_base() {
+        return precio_base;
+    }
+
+    public void setPrecio_base(Double precio_base) {
+        this.precio_base = precio_base;
+    }
 }
