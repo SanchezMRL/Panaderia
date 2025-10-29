0983f39817d0786191898315da9a967695d62e8d
Sanchez Morales Jairo <165832316+SanchezMRL@users.noreply.github.com>
2025-10-28 19:27:33 -0500

    Update DetallePedidoClienteRepository.java

--- src/main/java/com/panaderia/repository/DetallePedidoClienteRepository.java
+++ src/main/java/com/panaderia/repository/DetallePedidoClienteRepository.java
@@ -3,5 +3,5 @@ package com.panaderia.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.panaderia.entity.DetallePedidoCliente;
 
-public interface DetallePedidoClienteRepository extends JpaRepository<DetallePedidoCliente, Integer> {
+public interface DetallePedidoClienteRepository extends JpaRepository<DetallePedidoCliente, Long> {
 }
