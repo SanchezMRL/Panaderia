2da2e6c313459e911c8eee4973d5004122da3b9d
Sanchez Morales Jairo <165832316+SanchezMRL@users.noreply.github.com>
2025-10-28 19:17:30 -0500

    Update MetodoPago.java

--- src/main/java/com/panaderia/entity/MetodoPago.java
+++ src/main/java/com/panaderia/entity/MetodoPago.java
@@ -8,13 +8,13 @@ public class MetodoPago {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer idMetodoPago;
+    private Long idMetodoPago;
 
     private String nombre;
 
     // Getters y Setters
-    public Integer getIdMetodoPago() { return idMetodoPago; }
-    public void setIdMetodoPago(Integer idMetodoPago) { this.idMetodoPago = idMetodoPago; }
+    public Long getIdMetodoPago() { return idMetodoPago; }
+    public void setIdMetodoPago(Long idMetodoPago) { this.idMetodoPago = idMetodoPago; }
 
     public String getNombre() { return nombre; }
     public void setNombre(String nombre) { this.nombre = nombre; }
