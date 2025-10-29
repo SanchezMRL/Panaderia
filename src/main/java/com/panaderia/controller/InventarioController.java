0d9b64c5d754c1681e1423124822154ff9812aaf
Sanchez Morales Jairo <165832316+SanchezMRL@users.noreply.github.com>
2025-10-28 23:47:27 -0500

    Update InventarioController.java

--- src/main/java/com/panaderia/controller/InventarioController.java
+++ src/main/java/com/panaderia/controller/InventarioController.java
@@ -3,7 +3,7 @@ package com.panaderia.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;   // ✅ Este es el que faltaba
+import org.springframework.web.bind.annotation.RequestMapping;   
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
