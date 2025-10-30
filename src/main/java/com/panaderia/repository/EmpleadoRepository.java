0e510597f1b8c7e92fd997d97c4b4df29b465d3a
Sanchez Morales Jairo <165832316+SanchezMRL@users.noreply.github.com>
2025-10-29 19:04:23 -0500

    Update EmpleadoRepository.java

--- src/main/java/com/panaderia/repository/EmpleadoRepository.java
+++ src/main/java/com/panaderia/repository/EmpleadoRepository.java
@@ -3,10 +3,14 @@ package com.panaderia.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.panaderia.entity.Empleado;
+import java.util.Optional;
 
 @Repository
 public interface EmpleadoRepository extends JpaRepository<Empleado, Long> {
 
-    // método: busca por email y password
+    // Para login
     Empleado findByEmailAndPassword(String email, String password);
+
+    // Para verificar si un correo ya existe antes de registrar
+    Optional<Empleado> findByEmail(String email);
 }
