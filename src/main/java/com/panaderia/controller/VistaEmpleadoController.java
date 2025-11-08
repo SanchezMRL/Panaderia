9a094159c7ea12ed50d5c50711fac49c58e00a48
Sanchez Morales Jairo <165832316+SanchezMRL@users.noreply.github.com>
2025-11-08 16:52:07 -0500

    Delete src/main/java/com/panaderia/controller/VistaEmpleadoController.java

--- src/main/java/com/panaderia/controller/VistaEmpleadoController.java
+++
@@ -1,9 +0,0 @@
-@Controller
-@RequestMapping("/empleados/agregar")
-public class VistaEmpleadoController {
-
-    @GetMapping
-    public String mostrarFormularioEmpleado() {
-        return "agregar"; // agregar.html
-    }
-}
