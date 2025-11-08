94ba813bf4534db942202757f3e17ee98a5d0ea7
Sanchez Morales Jairo <165832316+SanchezMRL@users.noreply.github.com>
2025-11-08 17:11:40 -0500

    Update Empleado.java

--- src/main/java/com/panaderia/entity/Empleado.java
+++ src/main/java/com/panaderia/entity/Empleado.java
@@ -13,9 +13,6 @@ public class Empleado {
     @Column(nullable = false)
     private String nombre;
 
-    @Column(nullable = false)
-    private String apellido;
-
     @Column(nullable = false, unique = true)
     private String email;
 
@@ -42,14 +39,6 @@ public class Empleado {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
     }
 
     public String getEmail() {
