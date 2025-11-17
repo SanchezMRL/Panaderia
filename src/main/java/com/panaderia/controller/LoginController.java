80ae0b5d3f6be39554249454e512aa4fd3e5c2bc
Sanchez Morales Jairo <165832316+SanchezMRL@users.noreply.github.com>
2025-11-17 17:24:16 -0500

    Create LoginController.java

---
+++ src/main/java/com/panaderia/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.panaderia.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String loginForm() {
+        return "login"; // ← carga login.html
+    }
+}
