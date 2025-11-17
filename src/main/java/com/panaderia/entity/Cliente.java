3a1aa92b975badb7e6dc10bad550377789c8210b
Sanchez Morales Jairo <165832316+SanchezMRL@users.noreply.github.com>
2025-11-17 17:13:57 -0500

    Update Cliente.java

--- src/main/java/com/panaderia/entity/Cliente.java
+++ src/main/java/com/panaderia/entity/Cliente.java
@@ -21,6 +21,9 @@ public class Cliente {
 
     private String password;
 
+    @Column(nullable = false)
+    private String rol;  // <<< NUEVO
+
     // --- Getters y Setters ---
 
     public Long getIdCliente() {
@@ -66,6 +69,19 @@ public class Cliente {
     public String getPassword() {
         return password;
     }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRol() {         
+        return rol;
+    }
+
+    public void setRol(String rol) { 
+        this.rol = rol;
+    }
+}
 
     public void setPassword(String password) {
         this.password = password;
