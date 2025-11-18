ec87fe3ca441e46d5be732631a7ba6ce451fed34
Sanchez Morales Jairo <165832316+SanchezMRL@users.noreply.github.com>
2025-11-17 19:33:00 -0500

    Update SecurityConfig.java

--- src/main/java/com/panaderia/config/SecurityConfig.java
+++ src/main/java/com/panaderia/config/SecurityConfig.java
@@ -1,18 +1,20 @@
 package com.panaderia.config;
 
+import com.panaderia.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;  
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import com.panaderia.service.CustomUserDetailsService;
 
 @Configuration
 public class SecurityConfig {
 
     @Autowired
-private CustomUserDetailsService customUserDetailsService;
+    private CustomUserDetailsService customUserDetailsService;
+
 
 
     @Bean
