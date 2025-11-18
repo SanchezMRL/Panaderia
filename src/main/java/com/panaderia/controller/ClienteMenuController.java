e48222356d4829b5021781b39e5590642c401629
Sanchez Morales Jairo <165832316+SanchezMRL@users.noreply.github.com>
2025-11-17 20:42:37 -0500

    Update ClienteMenuController.java

--- src/main/java/com/panaderia/controller/ClienteMenuController.java
+++ src/main/java/com/panaderia/controller/ClienteMenuController.java
@@ -8,13 +8,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ClienteMenuController {
 
-    @GetMapping("/clienteMenu")
-    public String mostrarMenuCliente(Authentication authentication, Model model) {
+    @Autowired
+    private ClienteRepository clienteRepository;
 
-        if (authentication != null) {
-            String email = authentication.getName();
-            model.addAttribute("email", email);
+    @GetMapping("/clienteMenu")
+    public String clienteMenu(Model model, Authentication auth) {
+
+        String email = auth.getName(); // email del usuario logueado
+
+        Cliente cliente = clienteRepository.findByEmail(email);
+
+        if (cliente == null) {
+            return "error"; // por seguridad
         }
+
+        model.addAttribute("cliente", cliente);
 
         return "clienteMenu";
     }
