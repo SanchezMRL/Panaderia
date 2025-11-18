757b61b6ddf30168c1e8428ad19299811f9ae4b8
Sanchez Morales Jairo <165832316+SanchezMRL@users.noreply.github.com>
2025-11-18 15:16:43 -0500

    Update ClienteRepository.java

--- src/main/java/com/panaderia/repository/ClienteRepository.java
+++ src/main/java/com/panaderia/repository/ClienteRepository.java
@@ -2,7 +2,8 @@ package com.panaderia.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.panaderia.entity.Cliente;
+import java.util.Optional;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-    Cliente findByEmail(String email);
+    Optional<Cliente> findByEmail(String email); 
 }
